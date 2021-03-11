5357b212f6c879e9dc689a7588ae9cda60d44629
Dan T <dp.tyler@hotmail.co.uk>
2021-03-11 13:05:49 +0000

    Adding passing tests to reverse TDD the program once I delete the existing code

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -11,23 +11,31 @@ class Shop {
   constructor(items=[]){
     this.items = items;
   }
+  // this method shouldn't be called update quality as it updates both quality and sellIn.
   updateQuality() {
+    // for loop to iterate through each item within the shop
     for (let i = 0; i < this.items.length; i++) {
+      // checking whether the item is NOT aged brie AND NOT backstage passes 
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+        // if item is NOT aged brie AND NOT backstage passes, it checks if items quality is greater than 0
         if (this.items[i].quality > 0) {
+          // if item is not aged brief and not backstage passes and if items quality is greater than 0, and if item is not legendary item, it reduces quality by 1 
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
             this.items[i].quality = this.items[i].quality - 1;
           }
         }
       } else {
+        // else, if item is brie or backstage passes. if the items quality is less than 50 it adds 1 to quality. 
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
+          // if item is backstage pass, if it's sell in is less than 11 and quality is less than 50, quality + 1  
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1;
               }
             }
+            // if item is backstage pass, if it's sell in is less than 6 and quality is less than 50, quality + 1 
             if (this.items[i].sellIn < 6) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1;
@@ -36,9 +44,12 @@ class Shop {
           }
         }
       }
+      // same overheda if statement but no focused on the sell-in. 
+      // if item is not legendary sulfuras, -1 off sellin value. 
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
+      // if sellin is less than 0, if item is not brie and not backstage pass, if item quality is more than 0 and item is not legendary sulfuras, then quality - 1
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
@@ -47,10 +58,12 @@ class Shop {
                 this.items[i].quality = this.items[i].quality - 1;
               }
             }
+            // if item is backstage pass and sellin is less than 0, item quality is effectively 0 
           } else {
             this.items[i].quality = this.items[i].quality - this.items[i].quality;
           }
         } else {
+          // if item is aged brie and quality is less than 50, quality + 1 
           if (this.items[i].quality < 50) {
             this.items[i].quality = this.items[i].quality + 1;
           }
@@ -66,8 +79,3 @@ module.exports = {
   Item,
   Shop
 };
-
-// export default {
-//   Item,
-//   Shop
-// };
--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,9 +1,53 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
 describe("Gilded Rose", function() {
-  it("should foo", function() {
+  
+  it("should create an item called foo", function() {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe("foo");
   });
+
+  describe('normal items', function(){
+    
+    it('should update a normal item', function(){
+      const gildedRose = new Shop([new Item('+5 Dexterity Vest', 10, 20)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(9);
+      expect(items[0].quality).toBe(19);
+    })
+
+  })
+
+  describe('aged brie', function(){
+
+    it('should update aged brief correctly', function(){
+      const gildedRose = new Shop([new Item('Aged Brie', 2, 0)])
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(1);
+      expect(items[0].quality).toBe(1);
+    })
+
+  })
+
+  describe('backstage passes', function(){
+    it('should update backstage passes correctly', function(){
+      const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20)])
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(14);
+      expect(items[0].quality).toBe(21);
+    })
+  })
+  
+  describe('legendary items', function(){
+    
+      it('should update legendary items correctly', function(){
+        const gildedRose = new Shop([new Item('Sulfuras, Hand of Ragnaros', 0, 80)])
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).toBe(0);
+        expect(items[0].quality).toBe(80);
+      })
+    
+  })
+
 });
