c657b66687eb6b2bac4e787687356792cd64f2dd
Dan T <dp.tyler@hotmail.co.uk>
2021-04-06 15:07:19 +0100

    backstage class updated with magic numbers

--- src/backstage.js
+++ src/backstage.js
@@ -1,29 +1,37 @@
+const MIN_QUALITY = 0;
+const MAX_QUALITY = 50;
+const MAX_QUALITY_INCREASE = 3;
+const MID_QUALITY_INCREASE = 2;
+const NORM_QUALITY_INCREASE = 1;
+const MIN_SELL_IN = 0;
+const LOW_SELL_IN = 5;
+const MID_SELL_IN = 10;
+
 class Backstage {
   constructor(item) {
     this.updateBackstage = this.updateBackstageQuality(item);
   }
 
   updateBackstageQuality(item) {
-    if (item.sellIn < 0) {
-      item.quality = 0;
-    } else if (item.sellIn <= 5) {
-      this.increaseQuality(item, 3);
-    } else if (item.sellIn <= 10) {
-      this.increaseQuality(item, 2);
+    if (item.sellIn < MIN_SELL_IN) {
+      item.quality = MIN_QUALITY;
+    } else if (item.sellIn <= LOW_SELL_IN) {
+      this.increaseQuality(item, MAX_QUALITY_INCREASE);
+    } else if (item.sellIn <= MID_SELL_IN) {
+      this.increaseQuality(item, MID_QUALITY_INCREASE);
     } else {
-      this.increaseQuality(item, 1);
+      this.increaseQuality(item, NORM_QUALITY_INCREASE);
     }
     this.qualityCheck(item);
   }
 
   qualityCheck(item) {
-    item.quality >= 50 ? item.quality = 50 : item.quality;
+    item.quality >= MAX_QUALITY ? (item.quality = MAX_QUALITY) : item.quality;
   }
-  
+
   increaseQuality(item, num) {
     item.quality += num;
   }
-
 }
 
 module.exports = {
