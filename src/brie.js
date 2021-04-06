ca7e2591ef3257a0868a1e7c9f3241ccc2dc96a4
Dan T <dp.tyler@hotmail.co.uk>
2021-04-06 15:08:29 +0100

    brie class updated with magic numbers

--- src/brie.js
+++ src/brie.js
@@ -1,12 +1,15 @@
+const MAX_QUALITY = 50;
+const NORM_QUALITY_INCREASE = 1;
+
 class Brie {
   constructor(item) {
     this.updateBrie = this.updateBrieQuality(item);
   }
 
   updateBrieQuality(item) {
-    if (item.quality == 50) {
+    if (item.quality == MAX_QUALITY) {
     } else {
-      this.increaseQuality(item, 1);
+      this.increaseQuality(item, NORM_QUALITY_INCREASE);
     }
   }
 
